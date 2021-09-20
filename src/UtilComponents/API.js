78032b53b9cd03e96c4111fa17fd9f7684c27b6a
MrAMY-B <amolbharsakle1@gmail.com>
2021-09-20 10:32:52 +0530

    Added Shelf Option

--- src/UtilComponents/API.js
+++ src/UtilComponents/API.js
@@ -1,3 +1,3 @@
-export const LOCAL_API = "http://localhost:8080"
+export const LOCAL_API = "http://localhost:8080/"
 export const HEROKU_API = "https://amol-bookworm-api.herokuapp.com"
 export const API = "https://amol-bookworm-api.herokuapp.com"
